09153cf48ffadd55e369f70af5e5a199b3e0f106
Yi-Hsuan Kuo <y3kuo@ucsd.edu>
2026-04-15 19:01:51 -0700

    root pnl:7383, file pepper_root.py, also update to merged_trader_osmium_replaced.py

--- round_1/merged_trader_osmium_replaced.py
+++ round_1/merged_trader_osmium_replaced.py
@@ -167,13 +167,30 @@ class Trader:
             # ---------------- INTARIAN_PEPPER_ROOT ----------------
             if product == "INTARIAN_PEPPER_ROOT":
                 if current_position < limit and max_buy > 0:
-                    remaining = max_buy
+                    remaining_to_buy = max_buy
+                    
+                    # 1. AGGRESSIVE BUYING (with a price cap)
+                    # Only buy asks that are at or just 1 tick above the best_ask
+                    acceptable_ask = best_ask + 1 
+                    
                     for ask_price in sorted(order_depth.sell_orders.keys()):
-                        if remaining <= 0:
+                        if remaining_to_buy <= 0:
                             break
-                        vol = min(remaining, -order_depth.sell_orders[ask_price])
+                        # Stop buying if the order book gets too expensive
+                        if ask_price > acceptable_ask:
+                            break
+                            
+                        vol = min(remaining_to_buy, -order_depth.sell_orders[ask_price])
                         orders.append(Order(product, ask_price, vol))
-                        remaining -= vol
+                        remaining_to_buy -= vol
+
+                    # 2. PASSIVE BUYING
+                    # If we haven't reached our limit of 80, place a passive bid
+                    # to try and get filled at a cheaper price
+                    if remaining_to_buy > 0:
+                        # Place a bid 1 tick below the best ask (or at the best bid)
+                        passive_bid_price = best_ask - 1
+                        orders.append(Order(product, passive_bid_price, remaining_to_buy))
 
             # ---------------- ASH_COATED_OSMIUM ----------------
             elif product == self.ASH_PRODUCT:
